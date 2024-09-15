712f14c1e3710c1ed0e02d1affb5071d652294de
ngonis <ngonis@afrosoft.co.zw>
2024-09-15 04:07:18 +0200

    files

---
+++ Administration/src/test/java/zw/co/nseremwe/AdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.nseremwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
